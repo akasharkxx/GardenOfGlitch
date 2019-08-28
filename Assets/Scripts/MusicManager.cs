f27c0d2d27e1e45b437d5c84546108d5cf533f32
Akash Jaiswar <akash.jaiswar41@gmail.com>
2019-08-29 02:57:26 +0530

    Added Persistent Music GameObject
    Added new GameObject and new Script Music Manager it helps to load music for different levels indexed using build indexing. Made UI more responsive with taking care of silent areas

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip[] LevelMusicChangeArray;
+    private AudioSource audioSource;
+
+    void Awake() {
+        DontDestroyOnLoad(gameObject);
+        Debug.Log("Dont Destroy On Load" + name);
+    }
+    void Start(){
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnLevelWasLoaded(int level){
+        AudioClip thisLevelMusic = LevelMusicChangeArray[level];
+
+        Debug.Log("Playing clip : " + thisLevelMusic);
+
+        if(thisLevelMusic){
+            //if some music is attached
+            audioSource.clip = thisLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play(); 
+        }
+    }
+}
